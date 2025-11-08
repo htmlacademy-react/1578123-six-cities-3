fba9d28491c2b86900278e6dc2e0946b4d51bd5a
Anton Vakhania <vakhaniaanton@gmail.com>
2025-11-09 00:20:52 +0300

    Добавляет решение для задания 'Контроль и ограничения (часть 2)'

--- src/store/action/action.ts
+++ src/store/action/action.ts
@@ -1,4 +1,6 @@
 import { createAction } from '@reduxjs/toolkit';
+import { USortingOptionValue } from '../../components/const/const';
 
 export const changeCity = createAction<string>('city/change');
 export const loadOffersList = createAction('offers/load');
+export const changeSort = createAction<USortingOptionValue | undefined>('sort/change');
