d5b4a06254806a45ec12ce08807fc1ebd60fcf35
Anton Vakhania <vakhaniaanton@gmail.com>
2025-11-16 18:19:37 +0300

    Добавляет решение для задания 'Прикрой меня тестом'

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -3,7 +3,7 @@ import { MainPage } from '../pages/main-page';
 import { LoginPage } from '../pages/login-page';
 import { FavoritesPage } from '../pages/favorites-page';
 import { OfferPage } from '../pages/offer-page';
-import { NotFoundPage } from '../pages/404-page';
+import { NotFoundPage } from '../pages/not-found-page';
 import PrivateRoute from '../components/private-route';
 import Layout from '../components/layout';
 import { AppRoute } from '../constants';
